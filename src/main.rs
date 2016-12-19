e40ca0e8dbe7a5e0c6fcc6215f9d7f654565b609
John Blomberg <johnblo@kth.se>
2016-12-19 03:02:43 +0100

    Rendering rectangle around a sinus curve

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ extern crate piston_window;
 extern crate rand;
 
 use piston_window::*;
-use rand::Rng;
-use rand::ThreadRng;
 
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
@@ -11,17 +9,37 @@ const BLOCK_SIZE: f64 = 10.0;
 const X_NUM_BLOCKS: u32 = SCREEN_WIDTH / BLOCK_SIZE as u32;
 const Y_NUM_BLOCKS: u32 = SCREEN_HEIGHT / BLOCK_SIZE as u32;
 
-fn render<E>(e: &E, window: &mut PistonWindow)
+struct Data {
+    t: f32,
+    x: u32,
+    y: u32,
+}
+
+fn get_y(f: f32, a: f32, t: f32) -> u32 {
+    let s = (f * t).sin() * a;
+    let normalized = (s + 1.0) / 2.0;
+    Y_NUM_BLOCKS - (Y_NUM_BLOCKS as f32 * normalized).floor() as u32
+}
+
+fn get_x(t: f32) -> u32 {
+    (X_NUM_BLOCKS as f32 * t).floor() as u32
+}
+
+fn render<E>(e: &E, window: &mut PistonWindow, t: f32, f: f32, a: f32)
     where E: GenericEvent
 {
-    let mut rng = rand::thread_rng();
+    let d = Data {
+        t: t,
+        x: get_x(t),
+        y: get_y(f, a, t),
+    };
 
     window.draw_2d(e, |c, g| {
         clear([0.0; 4], g);
         for y in 0..Y_NUM_BLOCKS {
             for x in 0..X_NUM_BLOCKS {
                 let pos = [BLOCK_SIZE * x as f64, BLOCK_SIZE * y as f64];
-                render_square(&c, g, color(x, y, &mut rng), pos)
+                render_square(&c, g, color(x, y, &d), pos)
             }
         }
     });
@@ -34,29 +52,76 @@ fn render_square<G: Graphics>(c: &Context, g: &mut G, color: [f32; 4], pos: [f64
               g);
 }
 
-fn color(x: u32, y: u32, rng: &mut ThreadRng) -> [f32; 4] {
-    let r = (1.0 / X_NUM_BLOCKS as f32) * x as f32;
-    let b = (1.0 / Y_NUM_BLOCKS as f32) * y as f32;
-    let g: f32 = rng.gen();
-    [r, g, b, 1.0]
+fn color(x: u32, y: u32, d: &Data) -> [f32; 4] {
+    let path = d.x == x && d.y == y;
+
+    let r = if path {
+        1.0 - d.t
+    } else {
+        (1.0 / X_NUM_BLOCKS as f32) * x as f32
+    };
+
+    let b = if path {
+        1.0 * d.t
+    } else {
+        (1.0 / Y_NUM_BLOCKS as f32) * y as f32
+    };
+
+    [r, 0.0, b, 1.0]
 }
 
 fn main() {
     let mut window: PistonWindow = WindowSettings::new("Rogue", [SCREEN_WIDTH, SCREEN_HEIGHT])
         .exit_on_esc(true)
-        // .fullscreen(true)
+        .fullscreen(true)
         .vsync(true)
         .build()
         .unwrap();
+    let mut t: f32 = 0.0;
+    let mut dt: f32 = 0.0005;
+    let mut f: f32 = 16.0;
+    let mut a: f32 = 0.5;
 
     while let Some(e) = window.next() {
         if let Some(button) = e.press_args() {
             match button {
                 Button::Keyboard(Key::Q) => break,
+                Button::Keyboard(Key::F) => {
+                    f += 0.5;
+                    t = 0.0
+                }
+                Button::Keyboard(Key::V) => {
+                    f -= if f <= 0.0 { 0.0 } else { 0.5 };
+                    t = 0.0
+                }
+                Button::Keyboard(Key::A) => {
+                    a += 0.025;
+                    t = 0.0
+                }
+                Button::Keyboard(Key::Z) => {
+                    a -= if a <= 0.0 { 0.0 } else { 0.025 };
+                    t = 0.0
+                }
+                Button::Keyboard(Key::S) => {
+                    dt += 0.0005;
+                    t = 0.0
+                }
+                Button::Keyboard(Key::X) => {
+                    dt -= if dt <= 0.0 { 0.0 } else { 0.0005 };
+                    t = 0.0
+                }
+                Button::Keyboard(Key::R) => {
+                    t = 0.0;
+                    dt = 0.0005;
+                    f = 16.0;
+                    a = 0.5;
+                }
                 _ => {}
             }
         }
-        render(&e, &mut window);
+        render(&e, &mut window, t, f, a);
+        t += dt;
+        t %= 1.0;
     }
 
 }
